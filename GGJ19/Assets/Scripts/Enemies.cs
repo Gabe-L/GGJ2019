6e610987724988d29656375d4d01e096db0b2282
Jason Tennent <tennentjason@gmail.com>
2019-01-26 20:38:40 +0000

    Made Enemy prefab and made changes to camera in Light Test scene.

---
+++ GGJ19/Assets/Scripts/Enemies.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+
+    public GameObject Enemy;
+    public float repeatTime = 10f;
+
+
+
+
+
+    private void Start()
+    {
+        InvokeRepeating("Enemy", 10f, repeatTime);
+    }
+
+    void Spawn()
+    {
+        Instantiate(Enemy, transform.position, Quaternion.identity);
+        Debug.Log("Enemy has spawned");
+    }
+
+
+
+
+
+}
+
+    
+
+
